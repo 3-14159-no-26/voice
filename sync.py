e4cf38b9121b2b36566962202f231583581c8e2e
jodon920502 <jordon920502@gmail.com>
2024-05-30 17:51:08 +0800

    更新 Sync新增同步步數

--- sync.py
+++ sync.py
@@ -49,7 +49,7 @@ Account_id = cursor.fetchall()
 print("Account id : ", Account_id)
 
 
-def LineNotify(startTime, nowTime, lowHeartRate, highHeartRate, avgHeartRate):
+def LineNotifyHeart(startTime, nowTime, lowHeartRate, highHeartRate, avgHeartRate):
     url = "https://notify-api.line.me/api/notify"
     token = line_notify_token
     headers = {"Authorization": "Bearer " + token}
@@ -68,11 +68,30 @@ def LineNotify(startTime, nowTime, lowHeartRate, highHeartRate, avgHeartRate):
     requests.post(url, headers=headers, data=data)
 
 
+def LineNotifyStep(startTime, nowTime, step):
+    goal = 3000
+    message = "\n" + startTime.strftime("%Y-%m-%d %H:%M:%S") + "\n" + \
+        nowTime.strftime("%Y-%m-%d %H:%M:%S") + "\n目前步數: " + str(step)
+    if step >= goal:
+        print("恭喜達成目標 " + str(goal) + " 步")
+        message += "\n恭喜達成目標 " + str(goal) + " 步"
+    url = 'https://notify-api.line.me/api/notify'
+    token = line_notify_token
+    headers = {
+        'Authorization': 'Bearer ' + token
+    }
+    data = {
+        'message': message
+    }
+    requests.post(url, headers=headers, data=data)
+
+
 # 更新 access_token
 def UpdateAccessToken(Account_id, Account_access_token):
     print("更新 access_token")
     # 查詢 Account 的 refresh_token
-    cursor.execute("SELECT `refresh_token` FROM `Account` WHERE `id` = %s", Account_id)
+    cursor.execute(
+        "SELECT `refresh_token` FROM `Account` WHERE `id` = %s", Account_id)
     Account_refresh_token = cursor.fetchone()
     print(
         "Account_id : %s " % Account_id,
@@ -99,11 +118,12 @@ def UpdateAccessToken(Account_id, Account_access_token):
     db.commit()
 
 # UpdateAccessToken("xxxo", google_refresh_token)
-
 # 無限迴圈 定時執行 一天一次 每天 00:00:00
 
-while True:
-    nowTime = datetime.now(timezone(timedelta(hours=8))) - timedelta(days=1)  # 再減一天
+
+def Heart():
+    nowTime = datetime.now(timezone(timedelta(hours=8))
+                           ) - timedelta(days=1)  # 再減一天
     print(nowTime.strftime("%Y-%m-%d %H:%M:%S"))
     # 每半小時執行一次 00 or 30
     # if date.strftime("%M") == "00" or date.strftime("%M") == "30":
@@ -128,10 +148,10 @@ while True:
 
         # startTimeMillis = datetime(2023, 11, 6, 0, 0, 0, 0, timezone(timedelta(hours=8))).timestamp() * 1000
         # endTimeMillis = startTimeMillis + 1800000
-
+        
         print("開始時間: ", startTimeMillis)
         print("結束時間: ", endTimeMillis)
-
+        
         body = {
             "aggregateBy": [
                 {
@@ -142,7 +162,7 @@ while True:
             "startTimeMillis": startTimeMillis,
             "endTimeMillis": endTimeMillis,
         }
-
+        
         data = requests.post(
             "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
             headers=headers,
@@ -158,7 +178,7 @@ while True:
                 in data["error"]["message"]
             ):
                 print("access_token 過期")
-
+                
                 # 查詢 Account 的 refresh_token
                 cursor.execute(
                     "SELECT `refresh_token` FROM `Account` WHERE `id` = %s",
@@ -169,10 +189,10 @@ while True:
                     "Account_id : %s " % Account_id[i],
                     "Account_refresh_token : %s " % Account_refresh_token,
                 )
-
+                
                 # 更新 access_token
                 UpdateAccessToken(Account_id[i][0], Account_refresh_token[0])
-
+                
             print("#" * 100)
             continue
 
@@ -213,12 +233,12 @@ while True:
             if lowHeartRate < 60 or highHeartRate > 100:
                 print("心率異常")
                 # 發送 LINE Notify
-                LineNotify(
+                LineNotifyHeart(
                     startTime, nowTime, lowHeartRate, highHeartRate, avgHeartRate
                 )
                 warn = 1
             else:
-                LineNotify(
+                LineNotifyHeart(
                     startTime, nowTime, lowHeartRate, highHeartRate, avgHeartRate
                 )
                 print("心率正常")
@@ -239,13 +259,105 @@ while True:
         else:
             print("數據為空")
         print("*" * 100)
-    # else:
-    #     print("非執行時間")
-    # 每秒印出一個星號
 
-    for i in range(0, 55):
-        print("*", end="", flush=True)
-        time.sleep(1)
 
-# 关闭数据库连接
-db.close()
+def Step():
+    nowTime = datetime.now(timezone(timedelta(hours=8)))
+    print(nowTime.strftime("%Y-%m-%d %H:%M:%S"))
+    # 每半小時執行一次 00 or 30
+    # if date.strftime("%M") == "00" or date.strftime("%M") == "30":
+    for i in range(0, Account_count[0]):
+        # 查詢 Account 的 access_token
+        cursor.execute(
+            "SELECT `access_token` FROM `Account` WHERE `id` = %s", Account_id[i]
+        )
+        Account_access_token = cursor.fetchone()
+        print("Account access_token : %s " % Account_access_token)
+        print(type(Account_access_token), "+", Account_access_token)
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + Account_access_token[0],
+        }
+        # 今天 00:00:00
+        startTime = nowTime.replace(
+            hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
+        startTimeMillis = int(startTime.timestamp() * 1000)
+        endTimeMillis = int(nowTime.timestamp() * 1000)
+        # startTimeMillis = datetime(2023, 11, 6, 0, 0, 0, 0, timezone(timedelta(hours=8))).timestamp() * 1000
+        # endTimeMillis = startTimeMillis + 1800000
+        print("開始時間: ", startTimeMillis)
+        print("結束時間: ", endTimeMillis)
+        body = {
+            "aggregateBy": [
+                {
+                    "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps",
+                },
+            ],
+            "bucketByTime": {"durationMillis": 86400000},
+            "startTimeMillis":  startTimeMillis,
+            "endTimeMillis": endTimeMillis,
+        }
+        data = requests.post(
+            "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
+            headers=headers,
+            json=body,
+        ).json()
+        print(data)
+        if 'error' in data:
+            print("error")
+            print(data['error']['code'])
+            if (
+                "Request had invalid authentication credentials. Expected OAuth 2 access token, login cookie or other valid authentication credential"
+                    in data['error']['message']):
+                print("access_token 過期")
+            print("#"*100)
+            continue
+        print("#"*100)
+        print("Account_id", Account_id[i][0])
+        uid = uuid.uuid4()
+        print("data['bucket'][0]['dataset'][0]['point'] : ",
+              data['bucket'][0]['dataset'][0]['point'])
+        if data['bucket'][0]['dataset'][0]['point'] != []:
+            print(str(uid))
+            # 查詢 此 id 的 userId
+            cursor.execute(
+                "SELECT `userId` FROM `Account` WHERE `id` = %s", Account_id[i][0])
+            Account_userId = cursor.fetchone()
+            print("Account userId : %s " % Account_userId)
+            cursor.execute(
+                "SELECT `userId` FROM `Account` WHERE `id` = %s", Account_id[i][0])
+            Account_userId = cursor.fetchone()
+            print("Account userId : %s " % Account_userId)
+            step = int(data['bucket'][0]['dataset'][0]
+                       ['point'][0]['value'][0]['intVal'])
+            # LINE Notify
+            # 若 目前時間 是 00:00
+            print("目前時間: ", nowTime.strftime("%H:%M"))
+            if nowTime.strftime("%H:%M") == "00:00":
+                LineNotifyStep(startTime, nowTime, step)
+            cursor.execute(
+                "INSERT INTO `step` (`id`, `userId`, `steps`, `timestamp`) VALUES (%s, %s, %s, %s)",
+                (
+                    str(uid),
+                    Account_userId[0],
+                    step,
+                    startTime.strftime("%Y-%m-%d %H:%M:%S"),
+                ),
+            )
+            db.commit()
+        else:
+            print("data['bucket'][0]['dataset'][0]['point'] is []")
+        print("*"*100)
+
+if __name__ == "__main__":
+    while True:
+        try:
+            Heart()
+            Step()
+            for i in range(0, 55):
+                print("*", end="", flush=True)
+                time.sleep(1)
+        except KeyboardInterrupt:
+            print("手動結束程式")
+            break
+            db.close()
